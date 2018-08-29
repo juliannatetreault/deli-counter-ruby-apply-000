c50d493566e35001153e9a349e156fe8e5d4c94a
Julianna Tetreault <juliannatetreault@icloud.com>
2018-08-29 05:12:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,11 +15,10 @@ def take_a_number(katz_deli, name)
  puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.length == 0
   puts "There is nobody waiting to be served!"
   else
-  katz_deli.each do |name|
   puts "Currently serving #{name[0]}."
   katz_deli.shift
 end
